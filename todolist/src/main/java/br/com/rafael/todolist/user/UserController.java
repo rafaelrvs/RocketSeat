83a0791ed0dd8047fb8a5bf58ac773de3612c3c3
rafaelrvs <rafaelvieira1333@gmail.com>
2023-10-09 11:47:00 -0300

    Adicionando controller Controller

---
+++ todolist/src/main/java/br/com/rafael/todolist/user/UserController.java
@@ -0,0 +1,29 @@
+package br.com.rafael.todolist.user;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Modiicador
+ * Public
+ * Private
+ * Protected
+ */
+
+ @RestController() //definindo controller
+ @RequestMapping("/users") // definindo rota
+public class UserController {
+  /*
+   * STRING
+   * UNTEGER
+   * DOUBLE
+   * FLOAT
+   * CHAR
+   * DATE
+   * VOID
+   */
+  public void create (){
+
+  }
+  
+}
---
+++ todolist/src/main/java/br/com/rafael/todolist/user/userModel.java
@@ -0,0 +1,9 @@
+package br.com.rafael.todolist.user;
+
+public class userModel {
+
+  public String username;
+  public String name;
+  public String password;
+  
+}
